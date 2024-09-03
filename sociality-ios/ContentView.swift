f3acbee6c9dd4f98dc99aa5a439d3d9747e0a6e0
Yaroslav Trach <zhbr282@gmail.com>
2024-09-03 14:22:02 -0400

    Initial Commit

---
+++ sociality-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sociality-ios
+//
+//  Created by User on 2024-09-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sociality-ios/sociality_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  sociality_iosApp.swift
+//  sociality-ios
+//
+//  Created by User on 2024-09-03.
+//
+
+import SwiftUI
+
+@main
+struct sociality_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
